32d5236ec85c013a9181b93553271dce5957f6ba
Quentin <quentin.geerts@bstorm.be>
2023-05-08 14:52:01 +0200

    Correction Exercice 03 - Chronomètre basique

--- src/app/exercices/exo03/exo03.component.ts
+++ src/app/exercices/exo03/exo03.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class Exo03Component {
 
+  count: number = 0;
+  timer: any;
+
+  start (): void {
+    if (this.timer) return;
+    this.timer = setInterval(() => { this.count++; }, 1000);
+  }
+
+  stop (): void {
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+
+  reset (): void {
+    this.count = 0;
+    this.stop();
+  }
+
+
+
+
 }
