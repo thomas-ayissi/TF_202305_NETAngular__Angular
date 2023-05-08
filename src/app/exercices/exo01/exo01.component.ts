cd0ed5777d17957fdd45f1fac25f68d6bd03f6c8
Quentin <quentin.geerts@bstorm.be>
2023-05-08 11:16:14 +0200

    Correction exercice 01

--- src/app/exercices/exo01/exo01.component.ts
+++ src/app/exercices/exo01/exo01.component.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class Exo01Component {
 
-  
+  nom: string = 'Geerts';
+  prenom: string = 'Quentin';
+  email: string = 'quentin.geerts@bstorm.be';
+  dateNaissance: Date = new Date(1996, 3, 3);
+  genre: string = 'homme';
+  langues: string[] = ['Français', 'Anglais'];
+  image?: string = 'https://www.presse-citron.net/app/uploads/2022/12/voie-de-leau-avatar-2.jpg'
 
 }
