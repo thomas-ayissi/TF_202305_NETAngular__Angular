bb00b6d7760407c4bcc663dd8ab1b04f881af2b6
Thomas Ayissi <thomas8sea@gmail.com>
2023-05-09 15:11:16 +0200

    brouillon exercice 7 - pour sauvegarde

--- src/app/exercices/exo07/exo07.component.ts
+++ src/app/exercices/exo07/exo07.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './exo07.component.html',
   styleUrls: ['./exo07.component.scss']
 })
+
 export class Exo07Component {
 
+  temperature1: number = 10;
+  temperature2: number = 10;
+
+  fahrenheit: number = (celsius * (9/5)) + 32
+
+  celsius: number = (33,8*fahrenheit -32) * (9/5)
 }
